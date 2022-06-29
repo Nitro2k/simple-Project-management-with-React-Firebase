0ab353babc9a50b2a4f942b916bbac5bd36132b5
Nuttachat <Nuttachat.su@gmail.com>
2022-06-30 01:12:06 +0700

    init commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,40 @@
+import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+//styles
+import './Sidebar.css'
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
+
+//components
+import Avatar from './Avatar'
+
+export default function Sidebar() {
+  const { user } = useAuthContext()
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                <img src={DashboardIcon} alt="dash icon" />
+                <span>DashBoard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="add icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
